47a03a7015a120329d3b78010a5a955e381ab73b
Nathan <nathan.lasorne@etud.u-picardie.fr>
2024-04-02 16:29:01 +0200

    Ajoutez des fichiers projet.

---
+++ Aire rectangle (challenge)/Program.cs
@@ -0,0 +1,11 @@
+﻿double AireRectangle(double L, double l)
+{
+    return L * l;
+}
+
+Console.WriteLine("Saisissez votre  longueur");
+double L = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Saisissez votre  largeur");
+double l = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"L'aire du rectangle est : {AireRectangle(L, l)}");    
